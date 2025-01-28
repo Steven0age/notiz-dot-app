fe79fe90e4db6ff3d61f36b9dd8e3b755aa42439
Stephan Haak <info@stephan-haak.com>
2025-01-28 20:26:49 +0100

    load stored notes from array + sort notes by date while loading

--- script.js
+++ script.js
@@ -3,25 +3,27 @@ const notes = [
     id: 1,
     title: "Testnotiz 1",
     content: "ich darf nicht vergessen, leckeren Tee einkaufen zu gehen",
-    lastUpdated: "DATUM/ZEIT",
+    lastUpdated: "1735744224000",
   },
   {
     id: 2,
     title: "Testnotiz 2",
     content: "Wäsche waschen - erst Weißwäsche und ggf. noch die Bunte",
-    lastUpdated: "DATUM/ZEIT",
+    lastUpdated: "1735833600000",
   },
   {
     id: 3,
     title: "Testnotiz 3",
     content: "Einkaufen: Milch, Eier, Sahne, Mehl, Bier, Bier, Bier",
-    lastUpdated: "DATUM/ZEIT",
+    lastUpdated: "1735826400000",
   },
 ];
 
 addEventListener("DOMContentLoaded", loadStoredNotes);
 
 function loadStoredNotes() {
+  sortNotes();
+
   notes.forEach((loadNote) => {
     const note = document.createElement("div");
     note.classList.add("preview-note");
@@ -36,7 +38,9 @@ function loadStoredNotes() {
 
     const meta = document.createElement("p");
     meta.classList.add("preview-meta");
-    const metaText = document.createTextNode(loadNote.lastUpdated);
+    const metaText = document.createTextNode(
+      convertDateTime(Number(loadNote.lastUpdated))
+    );
 
     headline.appendChild(headlineText);
     content.appendChild(contentText);
@@ -48,5 +52,23 @@ function loadStoredNotes() {
     console.log(note);
     const allNotes = document.getElementById("notes-preview-container");
     allNotes.appendChild(note);
+  });
+}
+
+function convertDateTime(input) {
+  const dateTime = new Date(input);
+  return dateTime.toLocaleString("de-DE", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+}
+
+function sortNotes() {
+  notes.sort((a, b) => {
+    return b.lastUpdated - a.lastUpdated;
   });
 }
